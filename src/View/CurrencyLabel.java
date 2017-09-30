3fc4da04eff896f50ef9b720a2d540d26d42c9a3
acuevas <apac.alexcuevas@gmail.com>
2017-09-30 15:06:56 +0200

    Ajout petites images

--- src/View/CurrencyLabel.java
+++ src/View/CurrencyLabel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class CurrencyLabel extends JLabel {
 
     public CurrencyLabel(String currency) {
-        super(new ImageIcon("../Currency_Converter/src/View/Images/" + currency + ".jpg"));
+        super(new ImageIcon("../Currency_Converter/src/View/Images/" + currency + "60.jpg"));
         this.setSize(new Dimension(60, 60));
         this.setPreferredSize(new Dimension(60, 60));
     }
--- src/View/WindowChooseCurrency.java
+++ src/View/WindowChooseCurrency.java
@@ -17,7 +17,7 @@ public class WindowChooseCurrency extends JFrame {
         jScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         jScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         this.setSize(new Dimension(415, 410));
-        main.setPreferredSize(new Dimension(390, 880));
+        main.setPreferredSize(new Dimension(390, 1005));
         this.add(jScrollPane);
         this.setVisible(true);
         this.setResizable(false);
@@ -28,9 +28,9 @@ public class WindowChooseCurrency extends JFrame {
     }
 
     private void initComponent() {
-        String[] strings = {"Australian dollar", "Brazilian real", "Canadian dollar", "Dinar", "Dollar", "Egyptian pound", "Euro", "Franc", "Gold ounce", "Icelandic krona",
-                "Indian rupee", "Japanese yen", "Mexican peso", "Pound", "Quatarian rial", "Russian ruble", "South-korean won",
-                "Swedish krona", "Swiss franc", "Vietnamese dong", "Yuan"};
+        String[] strings = {"Australian dollar", "Brazilian real", "Canadian dollar", "Cocaine", "Dinar", "Dollar", "Egyptian pound", "Epitech", "Euro",
+                "Franc", "Gold ounce", "Icelandic krona", "Indian rupee", "Japanese yen", "Kebab", "Mexican peso", "Pound", "Quatarian rial",
+                "Russian ruble", "South-korean won", "Swedish krona", "Swiss franc", "Vietnamese dong", "Yuan"};
         JButton image;
         for (String string : strings) {
             image = new JButton(new ImageIcon("../Currency_Converter/src/View/Images/" + string + ".jpg"));
